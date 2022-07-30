f15b6e509d20a04b3ef20042fbeccc70e10966ba
KiriraJames <jameskirira8@gmail.com>
2022-07-30 16:46:47 +0300

    Moved formations to formations.js helper file.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import React from 'react';
-import './assets/sass/App.scss';
 import FootballPitch from './components/FootballPitch'
 import InfoCard from './components/InfoCard'
+
+import Formations from './helpers/formations';
+
+import './assets/sass/App.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formation: '4-4-2',
+      formation: '4-5-1',
       formationPositions: {},
       selectedPosition: null,
     }
   }
 
+  
   setFormationPositions() {
-    switch(this.state.formation) {
 
-      case ('4-4-2'):
-          this.setState({
-              formationPositions: { 1: 'gk', 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb', 6: 'rcm', 7: 'rm', 8: 'lcm', 9: 'rs', 10: 'ls', 11: 'lm' },
-              // formationPositions: {
-              //     defence: { 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb' },
-              //     midfield: { 6: 'rcm', 7: 'rm', 8: 'lcm', 11: 'lm' },
-              //     attack: { 9: 'rs', 10: 'ls' }
-              // },
-          });
-          break;
-      default:
-          break;
-        
+    if ( ! Formations.hasOwnProperty( this.state.formation )) {
+      return
     }
 
+    this.setState({
+      formationPositions: Formations[this.state.formation].formationPositions
+    })
+
   }
+
+
 
   changeFormation(){
 
---
+++ src/helpers/formations.js
@@ -0,0 +1,16 @@
+export default {
+    '4-4-2' : {
+        name : "4-4-2",
+        formationPositions: { 1: 'gk', 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb', 6: 'rcm', 7: 'rm', 8: 'lcm', 9: 'rs', 10: 'ls', 11: 'lm' },
+    },
+
+    '4-4-2-D' : {
+        name : "4-4-2 Diamond",
+        formationPositions: { 1: 'gk', 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb', 6: 'cdm', 7: 'rcm', 8: 'cam', 9: 'rs', 10: 'ls', 11: 'lcm' },
+    },
+
+    '4-5-1' : {
+        name : "4-5-1",
+        formationPositions: { 1: 'gk', 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb', 6: 'rdm', 7: 'rw', 8: 'ldm', 9: 'cf', 10: 'cam', 11: 'lw' },
+    },
+}
