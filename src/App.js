ab6d1823fbdfe4939f5e79d7577246f4c40ad607
KiriraJames <jameskirira8@gmail.com>
2022-07-30 14:49:12 +0300

    Moving formation state to app.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,56 @@ class App extends React.Component {
     super(props);
     this.state = {
       formation: '4-4-2',
-      selectedPosition: null
+      formationPositions: {},
+      selectedPosition: null,
     }
+  }
+
+  setFormationPositions() {
+    switch(this.state.formation) {
+
+      case ('4-4-2'):
+          this.setState({
+              formationPositions: { 1: 'gk', 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb', 6: 'rcm', 7: 'rm', 8: 'lcm', 9: 'rs', 10: 'ls', 11: 'lm' },
+              // formationPositions: {
+              //     defence: { 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb' },
+              //     midfield: { 6: 'rcm', 7: 'rm', 8: 'lcm', 11: 'lm' },
+              //     attack: { 9: 'rs', 10: 'ls' }
+              // },
+          });
+          break;
+      default:
+          break;
+        
+    }
+
   }
 
   changeFormation(){
 
   }
 
+
+
+  componentDidMount() {
+    this.setFormationPositions();
+  }
+
+
   render() {
     return (
       <div className="App">
+
         Hello
-        <FootballPitch formation={this.state.formation} onFormationChange={this.changeFormation}/>
-        {/* <InfoCard /> */}
+
+        <FootballPitch 
+          formationPositions={this.state.formationPositions} 
+        />
+
+        {/* <InfoCard 
+          onFormationChange={this.changeFormation}
+        /> */}
+
       </div>
     );
   }
--- src/components/FootballPitch.js
+++ src/components/FootballPitch.js
@@ -2,37 +2,9 @@ import React from "react";
 import PositionMarker from './PositionMarker'
 
 class FootballPitch extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            formationPositions: {}
-        }
-    }
-
-    setFormation() {
-        switch(this.props.formation) {
-
-            case ('4-4-2'):
-                this.setState({
-                    formationPositions: ['gk', 'rb', 'lb', 'lcb', 'rcb', 'rcm', 'rm', 'lcm', 'rs', 'ls', 'lm'],
-                    // formationPositions: {
-                    //     defence: { 2: 'rb', 3: 'lb', 4: 'lcb', 5: 'rcb' },
-                    //     midfield: { 6: 'rcm', 7: 'rm', 8: 'lcm', 11: 'lm' },
-                    //     attack: { 9: 'rs', 10: 'ls' }
-                    // },
-                });
-                break;
-            default:
-                break;
-            
-        }
-
-        this.renderFormation();
-        
-    }
 
     renderFormation() {
-        if( this.state.formationPositions.length !== 11 ) {
+        if( Object.keys(this.props.formationPositions).length !== 11 ) {
             return
         }
 
@@ -42,14 +14,10 @@ class FootballPitch extends React.Component {
 
         return [...Array(11)].map((value, index) => 
                     
-            <PositionMarker key={index} number={index + 1} formationPosition={this.state.formationPositions[index]} />
+            <PositionMarker key={index} number={index + 1} formationPosition={ this.props.formationPositions[index + 1] } />
 
         )
 
-    }
-
-    componentDidMount() {
-        this.setFormation();
     }
 
     render() {
