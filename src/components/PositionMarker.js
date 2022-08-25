1e6de95711d344c6804169bdfec8359103e8a25e
KiriraJames <jameskirira8@gmail.com>
2022-08-25 08:37:33 +0300

    Displaying fallback if image logo cant load.

--- src/components/PositionMarker.js
+++ src/components/PositionMarker.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class PositionMarker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageCantDisplay: false
+        }
+
+        this.imageCantLoad = this.imageCantLoad.bind(this)
+
+    }
 
     getPlayerDisplayName(full_name) {
         let names_array = full_name.split(' ');
@@ -12,18 +21,43 @@ class PositionMarker extends React.Component {
         let display_name = names_array.join(' ');
         return display_name.toUpperCase();
     }
+
+    imageCantLoad() {
+        this.setState({
+            imageCantDisplay: true
+        })
+    }
     
     render() {
         return (
             <div className={ 'position-marker' + (this.props.isActive ? ' active' : '') + (this.props.player ? ' filled' : '') } data-formation-position={ this.props.formationPosition } onClick={ (e) => this.props.toggleInfoCard( {squad_number:this.props.number, formation_position:this.props.formationPosition}, e ) }>
                 
-                <div className={ this.props.player ? "player-badge" : "circle" }>
+                
+                {
+                    ( ! this.props.player || this.state.imageCantDisplay ) 
                     
-                    { this.props.number }
+                    && 
                     
-                    { this.props.player && <img src={this.props.player.team.crest} onError={ (e) => e.target.remove() } /> }
+                    <div className="circle">
 
-                </div>            
+                        { this.props.number }
+                        
+                    </div>
+                
+                }
+                
+                {
+                    ( this.props.player && ! this.state.imageCantDisplay )
+                    
+                    &&
+
+                    <div className="player-badge">
+                                        
+                        <img src={this.props.player.team.crest} onError={ this.imageCantLoad } />
+
+                    </div>
+
+                }
 
                 <div className="name-tag">
                     { this.props.player ? this.getPlayerDisplayName(this.props.player.name) : this.props.formationPosition.toUpperCase() }
