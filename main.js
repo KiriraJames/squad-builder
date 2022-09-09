70d39e09238b7d58deab811439edaad49b6c16fd
KiriraJames <jameskirira8@gmail.com>
2022-09-09 23:55:19 +0300

    Making changes for deployment.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
  
 const app = express()
 
